45d06919b28b3fdb1dbaa949c071c6182c27f083
Cory Parker <school.coryparker@gmail.com>
2021-01-19 12:09:32 -1000

    Add files via upload

--- clparker/src/edu/ics211/h01/Translator.java
+++ clparker/src/edu/ics211/h01/Translator.java
@@ -20,33 +20,44 @@ public class Translator implements Translate {
   }
 
 
-  @Override
+  /**
+   * Attempts to return a binary string from input stream
+   */
   public String asBinaryString(InputStream in) {
    try {
     // create BitReader from InputStream
     BitReader readr = new BitReader(in);
     
     // create StringBuilder/string
-   StringBuilder data = new StringBuilder();
+    StringBuilder data = new StringBuilder();
    
     // while loop until BitReader is done 
     while (readr.isDone()==false) {
-      data.append(readr.readAsInt());     
       //read bit from the bitreader
       //append  to StringBuilder
       //if bitreader is done, break loop
+      data.append(readr.readAsInt());     
+      
     }
+    //close
     readr.close();
-    return data.toString();
+    in.close();
     //return StringBuilder.toString
-  } catch (IOException e) {
+    return data.toString();
+   
+  } 
+   
+   catch (IOException e) {
     e.printStackTrace();
   }
+   
     return null;
   }
 
 
-  @Override
+  /**
+   * Attempts to return a hexadecimal string from input stream
+   */
   public String asHexadecimalString(InputStream in) {
     //Create an empty String or StringBuilder class.
     StringBuilder data = new StringBuilder();
@@ -55,37 +66,39 @@ public class Translator implements Translate {
       while (true) {//(in.read() != -1***
         // read byte from in
         int number = in.read();
-        System.out.println(number);
+        //if input stream is finished, break
         if (number==-1) {
           break;
         }
+        
         else {
-          String temp=Integer.toHexString(number);
           // convert byte to ( .toHexString[];)
           //append string to string builder. need 2 hex digits******
-          System.out.println(Integer.toString(number).length());
-          if (temp.length()==1) {//<2 doesn't work
-            System.out.println("add 0");
+          String temp=Integer.toHexString(number);
+          //if there is only 1 digit, add 0 then append
+          if (temp.length()==1) {
+            
             data.append(0);
           }
-         
-          data.append(temp);
-          
+         //append string to string builder
+          data.append(temp);  
         }
        
       }
-      //Return the String or StringBuilder.toString(). 
+      in.close();
+      //Return StringBuilder.toString(). 
       return data.toString();
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       e.printStackTrace();
     }
-
     
     return null;
   }
 
-
-  @Override
+  /**
+   * Attempts to return a hexadecimal string from input stream
+   */
   public String asUtf8String(InputStream in) {
     
     //create an array of bytes
@@ -97,14 +110,17 @@ public class Translator implements Translate {
         
         //create & return the newly constructed String
         String stringBytes = new String(byteArray);
+        in.close();
         return stringBytes;
         
-      } catch (IOException e) {
+      } 
+      catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       } 
+      
     return null;
-   
+    
   }
 
 }
