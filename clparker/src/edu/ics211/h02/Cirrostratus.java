f6d61de81565a72c1c7acccc5bc2c3ca1baa6950
Cory-PC <school.coryparker@gmail.com>
2021-01-21 22:20:52 -1000

    checkstyle fixes

--- clparker/src/edu/ics211/h02/Cirrostratus.java
+++ clparker/src/edu/ics211/h02/Cirrostratus.java
@@ -1,33 +1,47 @@
 /**
- * 
- */
+   * Creates a Cirrostratus cloud.
+   * @author Cory Parker
+   */
+
 package edu.ics211.h02;
 
 import java.util.Random;
 
 /**
+ * Cirrostratus Cloud.
  * @author Cory Parker
  *
  */
 public class Cirrostratus extends HighLevel {
 
   /**
-   * 
+   * sets the altitude to a valid random value for the cloud.
+   * @author Cory Parker
    */
   public Cirrostratus() {
     super(ranNum(), CloudShape.STRATO);
-    //A constructor with no parameters. set the altitude to a valid random value for the cloud
+    //A constructor with no parameters. 
     // set CloudShape correctly
   }
   
+  /**
+   * takes integer to set altitude for cloud.
+   * @author Cory Parker
+   * @param altitude from super
+   */
   public Cirrostratus (Integer altitude) {
     super(altitude, CloudShape.STRATO);
     //constructor with one parameter: altitude  
   }
   
+  /**
+   * generates random number.
+   * @author Cory Parker
+   * @return random number in range
+   */
   public static int ranNum() {
     Random random = new Random();
-    int x = random.nextInt(15000) + 20000;//change************
+    int x = random.nextInt(15000) + 20000;
     return x;
   }
 
--- clparker/src/edu/ics211/h02/Cirrus.java
+++ clparker/src/edu/ics211/h02/Cirrus.java
@@ -1,18 +1,21 @@
 /**
- * 
- */
+   * Creates a Cirrus cloud.
+   * @author Cory Parker
+   */
+
 package edu.ics211.h02;
 
 import java.util.Random;
 
 /**
+ * Cirrus Cloud.
  * @author Cory Parker
- *
  */
 public class Cirrus extends HighLevel {
 
   /**
-   * 
+   * sets the altitude to a valid random value for the cloud.
+   * @author Cory Parker
    */
   public Cirrus() {
     super(ranNum(), CloudShape.CIRRO);
@@ -20,15 +23,24 @@ public class Cirrus extends HighLevel {
     // set CloudShape correctly
   }
   
+  /**
+   * takes integer to set altitude for cloud.
+   * @author Cory Parker
+   * @param altitude from super
+   */
   public Cirrus (Integer altitude) {
     super(altitude, CloudShape.CIRRO);
-    //constructor with one parameter: altitude  
+    //constructor with one parameter altitude  
   }
   
+  /**
+   * generates random number.
+   * @author Cory Parker
+   * @return random number in range
+   */
   public static int ranNum() {
     Random random = new Random();
-    int x = random.nextInt(15000) + 20000;//change************
-    System.out.println("cirrus "+x);
+    int x = random.nextInt(15000) + 20000;
     return x;
   }
 
