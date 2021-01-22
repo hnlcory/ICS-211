895b773ad29192c6e2aabb868d56b15bdcbe3572
Cory-Mac <school.coryparker@gmail.com>
2021-01-21 22:41:11 -1000

    checkstyle updates

--- clparker/src/edu/ics211/h02/Altostratus.java
+++ clparker/src/edu/ics211/h02/Altostratus.java
@@ -1,27 +1,43 @@
 /**
- * 
- */
+   * Creates a Altostratus cloud.
+   * @author Cory Parker
+   */
+
 package edu.ics211.h02;
 
 import java.util.Random;
 
 /**
+ * Altostratus Cloud.
  * @author Cory Parker
  *
  */
 public class Altostratus extends MidLevel {
 
   /**
-   * 
+   * sets the altitude to a valid random value for the cloud.
+   * @author Cory Parker
    */
   public Altostratus() {
     super(ranNum(), CloudShape.STRATO);
-    // TODO Auto-generated constructor stub
+    //A constructor with no parameters. set the altitude to a valid random value for the cloud
+    // set CloudShape correctly
   }
+  
+  /**
+   * takes integer to set altitude for cloud.
+   * @author Cory Parker
+   * @param altitude from super
+   */
   public Altostratus(Integer altitude) {
     super(altitude, CloudShape.STRATO);
   }
   
+  /**
+   * generates random number.
+   * @author Cory Parker
+   * @return random number in range
+   */
   public static int ranNum() {
     Random random = new Random();
     int x = random.nextInt(20000 - 6501) + 6501;//possibly wrong
--- clparker/src/edu/ics211/h02/Cumulus.java
+++ clparker/src/edu/ics211/h02/Cumulus.java
@@ -1,18 +1,22 @@
 /**
- * 
- */
+   * Creates a Cumulus cloud.
+   * @author Cory Parker
+   */
+
 package edu.ics211.h02;
 
 import java.util.Random;
 
 /**
+ * Cumulus Cloud.
  * @author Cory Parker
  *
  */
 public class Cumulus extends LowLevel {
 
   /**
-   * 
+   * sets the altitude to a valid random value for the cloud.
+   * @author Cory Parker
    */
   public Cumulus() {
     super(ranNum(), CloudShape.CUMULO);
@@ -20,11 +24,21 @@ public class Cumulus extends LowLevel {
     // set CloudShape correctly
   }
   
+  /**
+   * takes integer to set altitude for cloud.
+   * @author Cory Parker
+   * @param altitude from super
+   */
   public Cumulus (Integer altitude) {
     super(altitude, CloudShape.CUMULO);
     //constructor with one parameter: altitude  
   }
   
+  /**
+   * generates random number.
+   * @author Cory Parker
+   * @return random number in range
+   */
   public static int ranNum() {
     Random random = new Random();
     int x = random.nextInt(6500);
