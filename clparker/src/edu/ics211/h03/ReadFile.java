c6193594408bed94237d1f2a4014c58d05119e97
Cory-PC <school.coryparker@gmail.com>
2021-01-27 20:13:12 -1000

    working on first attempt!

--- clparker/src/edu/ics211/h03/ReadFile.java
+++ clparker/src/edu/ics211/h03/ReadFile.java
@@ -6,6 +6,7 @@ package edu.ics211.h03;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Cory Parker
@@ -23,39 +24,39 @@ public class ReadFile implements IReadFile {
 
   @Override
   public String readFile(String fileName) throws IOException {
-    
-    DataInputStream dataIn = new DataInputStream(new FileInputStream(fileName)); //create data input stream from a file input stream from file name
+
+    DataInputStream dataIn = new DataInputStream(new FileInputStream(fileName)); // create data input stream from a file
+                                                                                 // input stream from file name
     int numBytes = dataIn.readInt();// read in number of bytes (an integer) readInt
-    int encoding = dataIn.readByte();//read in encoding (a byte) readByte
-    byte[] bytes = new byte[numBytes];//create a byte[] of length numBytes
-    
-    dataIn.read(bytes);//fill array from DataInputStream
-    
+    int encoding = dataIn.readByte();// read in encoding (a byte) readByte
+    byte[] bytes = new byte[numBytes];// create a byte[] of length numBytes
+
+    dataIn.read(bytes);// fill array from DataInputStream
+
     switch (encoding) {
       case 1:
-        //StandardCharsets.US_ASCII
+        return new String(bytes, StandardCharsets.US_ASCII);
+      // StandardCharsets.US_ASCII
       case 2:
-        //StandardCharsets.UTF_16LE
+        return new String(bytes, StandardCharsets.UTF_16LE);
+      // StandardCharsets.UTF_16LE
       case 3:
-        //StandardCharsets.UTF_8
+        return new String(bytes, StandardCharsets.UTF_8);
+      // StandardCharsets.UTF_8
       case 4:
-        //StandardCharsets.UTF_16
+        return new String(bytes, StandardCharsets.UTF_16);
+      // StandardCharsets.UTF_16
+      default:
+        throw new IllegalArgumentException();
     }
-    
-    
-    //create data input stream from a file input stream from file name
+
+    // create data input stream from a file input stream from file name
     // read in number of bytes (an integer) readInt
-    //read in encoding (a byte) readByte
-    //create a byte[] of length numBytes
-    //fill array from DataInputStream
-    
-    //switch 1-4 (return new string ...)
-    
-    
-    
-    
-    // TODO Auto-generated method stub
-    return null;
+    // read in encoding (a byte) readByte
+    // create a byte[] of length numBytes
+    // fill array from DataInputStream
+    // switch 1-4 (return new string ...)
+
   }
 
 }
