f28488f01053996930f35dcc9ab8925643214053
Cory-PC <school.coryparker@gmail.com>
2021-01-27 19:15:23 -1000

    init

---
+++ clparker/src/edu/ics211/h03/ReadFile.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.ics211.h03;
+
+import java.io.IOException;
+
+/**
+ * @author Cory Parker
+ *
+ */
+public class ReadFile implements IReadFile {
+
+  /**
+   * 
+   */
+  public ReadFile() {
+    // TODO Auto-generated constructor stub
+  }
+
+
+  @Override
+  public String readFile(String fileName) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
