d134b44e0545f0e249094b47c87d913a9eb4ec5c
Cory-PC <school.coryparker@gmail.com>
2021-01-27 20:00:11 -1000

    updates

--- clparker/src/edu/ics211/h03/ReadFile.java
+++ clparker/src/edu/ics211/h03/ReadFile.java
@@ -3,6 +3,8 @@
  */
 package edu.ics211.h03;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -21,6 +23,37 @@ public class ReadFile implements IReadFile {
 
   @Override
   public String readFile(String fileName) throws IOException {
+    
+    DataInputStream dataIn = new DataInputStream(new FileInputStream(fileName)); //create data input stream from a file input stream from file name
+    int numBytes = dataIn.readInt();// read in number of bytes (an integer) readInt
+    int encoding = dataIn.readByte();//read in encoding (a byte) readByte
+    byte[] bytes = new byte[numBytes];//create a byte[] of length numBytes
+    
+    dataIn.read(bytes);//fill array from DataInputStream
+    
+    switch (encoding) {
+      case 1:
+        //StandardCharsets.US_ASCII
+      case 2:
+        //StandardCharsets.UTF_16LE
+      case 3:
+        //StandardCharsets.UTF_8
+      case 4:
+        //StandardCharsets.UTF_16
+    }
+    
+    
+    //create data input stream from a file input stream from file name
+    // read in number of bytes (an integer) readInt
+    //read in encoding (a byte) readByte
+    //create a byte[] of length numBytes
+    //fill array from DataInputStream
+    
+    //switch 1-4 (return new string ...)
+    
+    
+    
+    
     // TODO Auto-generated method stub
     return null;
   }
