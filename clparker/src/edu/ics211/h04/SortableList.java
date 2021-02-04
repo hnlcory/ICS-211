b4e41e138ea0a0de7fb7c630a4738e8198ccf0d2
Cory-PC <school.coryparker@gmail.com>
2021-02-03 22:29:03 -1000

    updates to format, junit updates

--- clparker/src/edu/ics211/h04/SortableList.java
+++ clparker/src/edu/ics211/h04/SortableList.java
@@ -6,38 +6,37 @@ package edu.ics211.h04;
 import java.util.Arrays;
 import java.util.Comparator;
 
-
-
 /**
  * @author Cory Parker
  *
  */
-public class SortableList<E> implements IList211<E>, ISortableList<E>{
+public class SortableList<E> implements IList211<E>, ISortableList<E> {
   private E[] data;
   private int size;
   private int swaps;
   private int comps;
   private double sortTime;
-  
+
   public SortableList() {
-    //init member variable
+    // init member variable
     this.data = (E[]) new Object[13];
   }
 
+
   @Override
   public void insertionSort(Comparator<E> compare) {
-    swaps=0;
-    comps=0;
-    //set swaps, comps to 0
-    //Dont loop to data.length-1, loop from 1 to size-1(nothing at size, we start at 0)
-    //dont have to pull data from sort record?, compare bc they are just E
-    
+    swaps = 0;
+    comps = 0;
+    // set swaps, comps to 0
+    // Dont loop to data.length-1, loop from 1 to size-1(nothing at size, we start at 0)
+    // dont have to pull data from sort record?
+
     int n = data.length;
     for (int fill = 0; fill < n - 1; fill++) {
       int posMin = fill;
       for (int next = fill + 1; next < n; next++) {
         comps++;
-        int result = compare.compare(data[next], data[posMin]);
+        int result = compare.compare(data[next], data[posMin]);// *
 
         if (result < 0) {
           swaps++;
@@ -47,19 +46,20 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
 
       E temp = data[fill];
       data[fill] = data[posMin];
-      data[posMin] = temp;
+      data[posMin] = temp;// *
 
     }
 
   }
 
+
   @Override
   public void bubbleSort(Comparator<E> compare) {
-    swaps=0;
-    comps=0;
+    swaps = 0;
+    comps = 0;
     boolean exchange = false;
     int pass = 0;
-    
+
     do { // do while loop
       exchange = false; // set we havent changed
       for (int i = 1; i < data.length - pass; i++) {
@@ -80,11 +80,12 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
     } while (exchange);
   }
 
+
   @Override
   public void selectionSort(Comparator<E> compare) {
-    swaps=0;
-    comps=0;
-   
+    swaps = 0;
+    comps = 0;
+
     for (int i = 0; i < data.length - 1; i++) {
       E posMin = data[i];// guess smallest is at [i]
 
@@ -101,58 +102,64 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
     }
   }
 
+
   @Override
   public int getNumberOfSwaps() {
-   return swaps;
+    return swaps;
   }
+
 
   @Override
   public int getNumberOfComparisons() {
-   return comps;
+    return comps;
   }
+
 
   @Override
   public double getSortTime() {
     return sortTime;
   }
 
-  
-  
 
   @Override
   public E get(int index) {
-   checkIndex(index);
-   return data[index];
+    checkIndex(index);
+    return data[index];
   }
-  
+
+
   private void checkIndex(int index) {
-    if (index<0 || index >= size) {
+    if (index < 0 || index >= size) {
       throw new IndexOutOfBoundsException();
     }
   }
 
+
   @Override
   public E set(int index, E element) {
     checkIndex(index);
-    E temp = data[index]; //remmeber value @ index
-   data[index]=element;// set value at index to element
-    return temp;//return remembered
+    E temp = data[index]; // remmeber value @ index
+    data[index] = element;// set value at index to element
+    return temp;// return remembered
   }
+
 
   @Override
   public int indexOf(Object obj) {// ***may need to change to E type
-    for (int i=0; i<size-1; i++) {//loop 0 to size -1      *** may be weird bc i=0
-      if (obj.equals(data[i])){//  if obj.equals(item at index)
-        return i; //return index
-      }    
+    for (int i = 0; i < size - 1; i++) {// loop 0 to size -1 *** may be weird bc i=0
+      if (obj.equals(data[i])) {// if obj.equals(item at index)
+        return i; // return index
+      }
     }
-    return -1;//not found
+    return -1;// not found
   }
+
 
   @Override
   public int size() {
-   return size;
+    return size;
   }
+
 
   @Override
   public boolean add(E e) {
@@ -160,40 +167,52 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
     return true;
   }
 
+
   @Override
   public void add(int index, E element) {
     if (index < 0 || index > size) { // if index is bad, throw exception
       throw new IndexOutOfBoundsException();
     }
     if (size == data.length) {// if data is full, grow
-      data = Arrays.copyOf(data, 2 * data.length);//grow array *2 to be efficient
+      data = Arrays.copyOf(data, 2 * data.length);// grow array *2 to be efficient
     }
+
     
-    for (int i=size-1 ;i>index ;i--) {//(loop size-1 to index) *possible wrong
-      data[i+1]=data[i];// shift all items from index to index+1 *may not work
+    
+    
+    for (int i = size - 1; i > index; i--) {// (loop size-1 to index) *possible wrong
+      data[i + 1] = data[i];// shift all items from index to index+1 *may not work
     }
-  
-   // for (int i=index ;i<size ;i++)
-        //data[i+1]=data[i]
+
+    // for (int i=index ;i<size ;i++)
+    // data[i+1]=data[i]
+
     
-   data[index]= element;//insert element into array at index
-   size++;//increment size
+    
+    
+    
+    data[index] = element;// insert element into array at index
+    size++;// increment size
   }
+
 
   @Override
   public E remove(int index) {
     checkIndex(index);
-    E temp = data[index];//remember item at index
+    E temp = data[index];// remember item at index
+
     
-   
-    for (int i=index;i<size-1;i++) { //shift items from index +1 to size of index *may be wrong
-      data[index]=data[index+1];//*may be wrong way
+    
+    for (int i = index; i < size - 1; i++) { // shift items from index +1 to size of index *may be wrong
+      data[index] = data[index + 1];// *may be wrong way
     }
+
     
     
-    size= size-1;//decrement size
-    return temp;//return remembered
     
+    size = size - 1;// decrement size
+    return temp;// return remembered
+
   }
 
 }
--- clparker/src/edu/ics211/h04/SortableListTest.java
+++ clparker/src/edu/ics211/h04/SortableListTest.java
@@ -24,7 +24,7 @@ class SortableListTest {
   @BeforeEach
   void setUp() throws Exception {
     sortedList.add(3);
-    sortedList.add(5);
+    sortedList.add(6);
     sortedList.add(10);
     sortedList.add(15);
   }
@@ -113,52 +113,95 @@ class SortableListTest {
 
   @Test
   void testGet() {
-    //create list
-    //add some items
+    SortableList<Integer> list = new SortableList<Integer>();//create list
+    list.add(3);//add some items
+    list.add(6);
+    list.add(10);
+    list.add(15);
+    
     //get good values
-    //assertEquals(val,list.get(0))
-    //assertEquals(val2, list.get(size-1))
-    
+    assertEquals(sortedList.get(0), list.get(0));
+    assertEquals(sortedList.get(3),list.get(list.size()-1));
+   
     //test some bad values
-    //try{
-    //list.get(-1)
-    // fail ("didnt throw exception for -1")
-    //}catch (IndexOutOfBoundsException ioobe){
-    //good
-    //}
-    
-  //try{
-    //list.get(size)
-    // fail ("didnt throw exception for size")
-    //}catch (IndexOutOfBoundsException ioobe){
-    //good
-    //}
-    fail("Not yet implemented");
+    try {
+      list.get(-1);
+      list.get(list.size());
+      fail("should throw IOOBE exception");
+    }
+    catch(IndexOutOfBoundsException ioobe){
+    }  
   }
 
 
   @Test
   void testSet() {
-    //similar to get
-    fail("Not yet implemented");
+    SortableList<Integer> list = new SortableList<Integer>();
+    list.add(5);//add some items
+    list.add(8);
+    list.add(20);
+    list.add(55);
+    
+    //good values
+    list.set(0, 3);
+    list.set(1, 6);
+    list.set(2, 10);
+    list.set(3, 15);
+    
+    //bad values
+    try {
+      list.set(-1, 5);
+      list.set(list.size(), 7);
+    }
+   catch (IndexOutOfBoundsException ioobe){  
+   }  
   }
 
 
   @Test
-  void testIndexOf() {
-    fail("Not yet implemented");
+  void testIndexOf() {//retuns size of list
+    SortableList<Integer> list = new SortableList<Integer>();//create list
+    list.add(3);//add some items
+    list.add(6);
+    list.add(10);
+    list.add(15);
+    //in list
+    assertEquals(list.indexOf(6), sortedList.indexOf(6));
+    assertEquals(list.indexOf(3), sortedList.indexOf(3));
+    assertEquals(list.indexOf(15), sortedList.indexOf(15));
+    //not in list
+    assertEquals(list.indexOf(25), sortedList.indexOf(25));
   }
 
 
   @Test
-  void testSize() {
-    fail("Not yet implemented");
+  void testSize() {//returns the size of list
+    SortableList<Integer> list = new SortableList<Integer>();//create list
+    list.add(3);//add some items
+    list.add(6);
+    list.add(10);
+    list.add(15);
+    
+    assertEquals(list.size(),sortedList.size());
+    
+    list.add(13);
+    try {
+      assertEquals(list.size(),sortedList.size());//should fail
+    }
+    catch(AssertionError AE){
+     
+    }
+   
   }
 
 
   @Test
-  void testAddE() {
-    fail("Not yet implemented");
+  void testAddE() {//Adds e to the end of the list.
+    SortableList<Integer> list = new SortableList<Integer>();
+    list.add(3);
+    list.add(6);
+   
+    assertEquals(list.indexOf(1),sortedList.indexOf(1));
   }
 
 
