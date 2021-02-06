ab0e8336a57046f2b35c3ca02d90b344864fe5af
Cory-Mac <school.coryparker@gmail.com>
2021-02-05 16:38:30 -1000

    init

---
+++ clparker/src/edu/ics211/h05/SortableList.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package edu.ics211.h05;
+
+import edu.ics211.h04.IList211;
+import edu.ics211.h04.ISortableList;
+import java.util.Comparator;
+
+/**
+ * @author hnlcory
+ *
+ */
+public class SortableList implements IList211, ISortableList{
+
+  @Override
+  public void insertionSort(Comparator compare) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void bubbleSort(Comparator compare) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void selectionSort(Comparator compare) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int getNumberOfSwaps() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int getNumberOfComparisons() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public double getSortTime() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public Object get(int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Object set(int index, Object element) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int indexOf(Object obj) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public boolean add(Object e) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void add(int index, Object element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public Object remove(int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
