322abc01cd2c23571bd62f976e184244268c2e8b
Cory-PC <school.coryparker@gmail.com>
2021-02-10 15:36:28 -1000

    some updates

--- clparker/src/edu/ics211/h05/SortableList.java
+++ clparker/src/edu/ics211/h05/SortableList.java
@@ -11,43 +11,43 @@ import java.util.Comparator;
  * @author Cory Parker
  *
  */
-public class SortableList<E> implements IList211<E>, ISortableList<E>{
+public class SortableList<E> implements IList211<E>, ISortableList<E> {
   private DLinkedNode tail;
   private int size;
   private int swaps;
   private int comps;
   private double sortTime;
-  
-  
+
   private class DLinkedNode {
     E item;
     DLinkedNode next;
     DLinkedNode prev;
-    
+
     public DLinkedNode(E item, DLinkedNode next, DLinkedNode prev) {
       this.item = item;
       this.next = next;
       this.prev = prev;
     }
   }
-  
+
   public SortableList() {
-    //init variables
-    tail=null;
-    size=0;
-    swaps=0;
-    comps=0;
-    sortTime=0;
-    
+    // init variables
+    tail = null;
+    size = 0;
+    swaps = 0;
+    comps = 0;
+    sortTime = 0;
+
   }
-  
+
+
   @Override
   public void insertionSort(Comparator<E> compare) {// *** all former data.lengths may be wrong
     swaps = 0;
     comps = 0;
     // set swaps, comps to 0
     long startTime = System.nanoTime();
-    int n = size ;// data.length switch to size of nodes?
+    int n = size;// data.length switch to size of nodes?
     for (int fill = 0; fill < n - 1; fill++) {
       int posMin = fill;
       for (int next = fill + 1; next < n; next++) {
@@ -73,6 +73,7 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
     this.sortTime = endTime - startTime;// subtract ending from starting to stortTime
   }
 
+
   @Override
   public void bubbleSort(Comparator<E> compare) {
     swaps = 0;
@@ -96,7 +97,7 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
           swaps++;// count swap
           E temp = get(i);
           set(i, get(i - 1));
-          set(i- 1, temp);
+          set(i - 1, temp);
           exchange = true;// change exchange to true
         }
       }
@@ -105,6 +106,7 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
     long endTime = System.nanoTime(); // get ending time from nanoTime
     this.sortTime = endTime - startTime;// subtract ending from starting to stortTime
   }
+
 
   @Override
   public void selectionSort(Comparator<E> compare) {
@@ -136,118 +138,159 @@ public class SortableList<E> implements IList211<E>, ISortableList<E>{
     this.sortTime = endTime - startTime;// subtract ending from starting to stortTime
   }
 
+
   @Override
   public int getNumberOfSwaps() {
     return swaps;
   }
+
 
   @Override
   public int getNumberOfComparisons() {
     return comps;
   }
 
+
   @Override
   public double getSortTime() {
     return sortTime;
   }
 
+
   @Override
   public E get(int index) {
     checkIndex(index);
     DLinkedNode temp = tail;
-    for (int i = size-1; i>=index;i++) {//may be > not >=
-       temp=temp.next;
+    for (int i = size - 1; i >= index; i++) {// may be > not >=
+      temp = temp.next;
     }
-    //return DLinkedNode.item;
+    // return DLinkedNode.item;
     return temp.item;
-    //traverse to index form tail (tail = size-1)
-    //return node.item
-   
+    // traverse to index form tail (tail = size-1)
+    // return node.item
+
   }
-  
+
+
   private void checkIndex(int index) {
     if (index < 0 || index >= size) {
       throw new IndexOutOfBoundsException();
     }
   }
 
+
   @Override
   public E set(int index, E element) {
     checkIndex(index);
     DLinkedNode temp = tail;
-    for (int i = size-1; i>=index;i++) {//may be > not >=
-     temp = temp.next;
+    for (int i = size - 1; i >= index; i++) {// may be > not >=
+      temp = temp.next;
     }
     E data = temp.item;
-    temp.item= element;
+    temp.item = element;
     return data;
-    //check index
-    //traverse from index to tail
-    //remmebr item in node
-    //set item in node to element
-    //return remembered
+    // check index
+    // traverse from index to tail
+    // remmebr item in node
+    // set item in node to element
+    // return remembered
   }
+
 
   @Override
   public int indexOf(Object obj) {
-    //keep track of index
-    //loop from tail to beginning
-    //  if note.item.equals(obj), return index
-    //  update index
-    //  update node
-    //return -1
-   return 0;
+    DLinkedNode temp = tail;
+    for (int index = 0; index >= size; index++) {// again >= to > or possibly <
+      if (temp.item.equals(obj)) {
+        return index;
+      }
+      temp = temp.next;
+    }
+    return -1;
+    // keep track of index
+    // loop from tail to beginning
+    // if note.item.equals(obj), return index
+    // update index***
+    // update node***
+    // return -1
+
   }
+
 
   @Override
   public int size() {
     return size;
   }
 
+
   @Override
   public boolean add(E e) {// Adds e to the end of the list.
-    add(size,e);
+    add(size, e);
     return true;
   }
 
-  @Override
-  public void add(int index, E element) {// Adds element to the list at the given index.
-   // if index is bad, size is ok throw index out of bounds
-   // if size is 0, point to new node (element,null,null)
-    //else if index is side, adding to the end
-   //if index is size
-   // create new DLinkedNode with element, null, tail
-    //if tail is not null
-    //tail.next = new node
-    //update tail to be new node
-   //else
-    //  traverse to index
-    //  create new node [element,temp, temp.prev] pointing new node to list
-    //  point list to new node
-    //  if temp.prev is not null
-    //    temp.prev.next = newNode
-    //  if temp.next is not null
-    //    temp.next.prev = newNode
-    //increment size
-    
-  }
 
   @Override
-  public E remove(int index) {//  Removes the element at the given index.
-   //check index
-   //traverse to index
-   //if index is size -1
-   //   make the list not point to the node
-   //   if temp.prev is not null
-   //     temp.prev.next=temp.next 
-   //   update tail
-   //else
-   //   make the list not point to the node
-    //  if temp.prev is not null
-    //    temp.prev.next = temp.next
-    //  if temp.next is not null
-    //    temp.next.prev = temp.prev
-    //decrement size
+  public void add(int index, E element) {// Adds element to the list at the given index.
+    if (index < 0 || index > size) {// if index is bad, size is ok throw index out of bounds
+      throw new IndexOutOfBoundsException();
+    }
+
+    if (size == 0) { // if size is 0, point to new node (element,null,null
+      tail = new DLinkedNode(element, null, null);
+    } 
+    else if (index == size) { // else if index is side, adding to the end
+      DLinkedNode temp = new DLinkedNode(element, null, tail);// create new DLinkedNode with element, null, tail
+      if (tail != null) {// if tail is not null
+        tail.next = temp;// tail.next = new node
+      }
+    }
+    
+    //disaster from here
+    else {
+      DLinkedNode temp = tail;
+      for (int i = 0; i < index - 1; i++) {
+        
+        DLinkedNode e = new DLinkedNode(element,temp,temp.prev);
+      }
+    }
+    // if index is bad, size is ok throw index out of bounds
+    // if size is 0, point to new node (element,null,null)
+    // else if index is side, adding to the end
+    // if index is size
+    // create new DLinkedNode with element, null, tail
+    // if tail is not null
+    // tail.next = new node
+    // update tail to be new node
+    // else
+    // traverse to index
+    // create new node [element,temp, temp.prev] pointing new node to list
+    // point list to new node
+    // if temp.prev is not null
+    // temp.prev.next = newNode
+    // if temp.next is not null
+    // temp.next.prev = newNode
+    // increment size
+
+  }
+
+
+  @Override
+  public E remove(int index) {// Removes the element at the given index.
+    // check index
+    // traverse to index
+    // if index is size -1
+    // make the list not point to the node
+    // if temp.prev is not null
+    // temp.prev.next=temp.next
+    // update tail
+    // else
+    // make the list not point to the node
+    // if temp.prev is not null
+    // temp.prev.next = temp.next
+    // if temp.next is not null
+    // temp.next.prev = temp.prev
+    // decrement size
     // return ?
     return null;
   }
