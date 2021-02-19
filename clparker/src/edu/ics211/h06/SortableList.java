dfb78c3034068492020a54571feba625424045a8
Cory-PC <school.coryparker@gmail.com>
2021-02-18 15:20:51 -1000

    working

--- clparker/src/edu/ics211/h06/SortableList.java
+++ clparker/src/edu/ics211/h06/SortableList.java
@@ -3,6 +3,7 @@
  */
 package edu.ics211.h06;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.ListIterator;
@@ -18,16 +19,18 @@ import edu.ics211.h04.IList211;
 public class SortableList<E> implements IList211<E>, Iterable<E> {
   private DLinkedNode tail;
   private int size;
-  int swaps = 0;//imports of sort
+  int swaps = 0;// imports of sort
   int comps = 0;
-  long sortTime =0;
+  long sortTime = 0;
+
   /**
    * 
    */
   public SortableList() {
-    tail=null;
-    size=0;
+    tail = null;
+    size = 0;
   }
+
 
   public void bubbleSort(Comparator<E> compare) {
     long startTime = System.nanoTime();
@@ -58,23 +61,26 @@ public class SortableList<E> implements IList211<E>, Iterable<E> {
     long endTime = System.nanoTime(); // get ending time from nanoTime
     this.sortTime = endTime - startTime;// subtract ending from starting to stortTime
   }
-  
-  
+
+
   @Override
   public Iterator<E> iterator() {
     return new MyListIterator();
   }
-  
-  public Iterator<E> iterator(Comparator<E> c){//import comparator?
-    return new MySortedIterator(c); 
+
+
+  public Iterator<E> iterator(Comparator<E> c) {// import comparator?
+    return new MySortedIterator(c);
   }
-  
+
+
   private void checkIndex(int index) {
     if (index < 0 || index >= size) {
       throw new IndexOutOfBoundsException();
     }
   }
-  
+
+
   @Override
   public E get(int index) {
     checkIndex(index);
@@ -83,7 +89,7 @@ public class SortableList<E> implements IList211<E>, Iterable<E> {
       temp = temp.prev;
 
     }
-    
+
     return temp.item;
     // traverse to index form tail (tail = size-1)
     // return node.item
@@ -155,7 +161,7 @@ public class SortableList<E> implements IList211<E>, Iterable<E> {
       for (int i = size - 1; i > index; i--) {
 
         temp = temp.prev;
-        System.out.println(temp.item);
+        //System.out.println(temp.item);
       }
       DLinkedNode e = new DLinkedNode(element, temp, temp.prev);
       if (temp.prev != null) {
@@ -202,202 +208,218 @@ public class SortableList<E> implements IList211<E>, Iterable<E> {
     E item;
     DLinkedNode next;
     DLinkedNode prev;
-    
+
     public DLinkedNode(E item, DLinkedNode next, DLinkedNode prev) {
       this.item = item;
       this.next = next;
       this.prev = prev;
     }
   }
-  
-  private class MyListIterator implements ListIterator<E>{
+
+  private class MyListIterator implements ListIterator<E> {
     private DLinkedNode nextNode;
     private int nextIndex;
 
-    public MyListIterator() {//?
-      nextIndex=0;
-      //nextNode=traverse[0];
+    public MyListIterator() {// ?
+      nextIndex = 0;
+      // nextIndex=0
+      nextNode = traverse(0);
     }
-    
+
+
+    private DLinkedNode traverse(int num) {
+      DLinkedNode temp = tail;
+      while (temp.prev != null) {
+        temp = temp.prev;
+      }
+      //System.out.println(temp.item);
+      return temp;
+    }
+
+
     @Override
     public boolean hasNext() {
-      if (nextNode != null) {  
+      if (nextNode != null) {
         return true;
       }
       return false;
     }
+
 
     @Override
     public E next() {
       if (hasNext() == true) {
         E data = nextNode.item;
-        nextNode= nextNode.next;//update nextNode?
-        nextIndex=nextIndex+1;//update nextIndex?
+        nextNode = nextNode.next;// update nextNode?
+        nextIndex = nextIndex + 1;// update nextIndex?
         return data;
-      }
-      else { 
+      } else {
         throw new NoSuchElementException();
       }
     }
 
+
     @Override
     public boolean hasPrevious() {
-      if (nextNode.prev != null || nextNode != null) {
+      if (nextNode == null ||nextNode.prev != null ) {
         return true;
       }
       return false;
     }
 
+
     @Override
     public E previous() {
       if (hasPrevious() == true) {
         E data = nextNode.prev.item;
-        nextNode = nextNode.prev;//update nextNode?
-        nextIndex=nextIndex-1;//update nextIndex?
+        nextNode = nextNode.prev;// update nextNode?
+        nextIndex = nextIndex - 1;// update nextIndex?
         return data;
-      }
-      else {
+      } else {
         throw new NoSuchElementException();
       }
     }
 
-    
+
     @Override
     public int nextIndex() {
       return nextIndex;
     }
 
+
     @Override
     public int previousIndex() {
-      return nextIndex-1;
+      return nextIndex - 1;
     }
+
 
     @Override
     public void remove() {
       throw new UnsupportedOperationException();
-      
+
     }
+
 
     @Override
     public void set(E e) {
       throw new UnsupportedOperationException();
-      
+
     }
+
 
     @Override
     public void add(E e) {
       throw new UnsupportedOperationException();
-      
+
     }
-    
+
   }
-  
-  private class MySortedIterator implements Iterator<E>{
+
+  private class MySortedIterator implements Iterator<E> {
     private SortableList<E> sorted;
     private int nextIndex;
 
     public MySortedIterator(Comparator<E> c) {
-      sorted=null;//init variables?
-      for (int i=0; i<size;i++) {
+      sorted= new SortableList<E>();// init variables?
+      for (int i = 0; i < size; i++) {
+        System.out.println(get(i));
+        System.out.println(i);
         sorted.add(get(i));
-        sorted.bubbleSort(c);//add in bubble sort or call?
-        nextIndex=0;
+       
       }
-      
-      //copy all items from this into sorted(for int i=0; i<size ; i++) sorted.add(get[i])
-      //  sort sorted (bubble sort)
-      //nextIndex=0
-      
+      sorted.bubbleSort(c);// add in bubble sort or call?
+      nextIndex = 0;
+      // copy all items from this into sorted(for int i=0; i<size ; i++) sorted.add(get[i])
+      // sort sorted (bubble sort)
+      // nextIndex=0
+
     }
+
+
     @Override
     public boolean hasNext() {
-      return nextIndex<size;
+      return nextIndex < size;
     }
+
 
     @Override
     public E next() {
       if (hasNext() == true) {
         return sorted.get(nextIndex++);
+      } else {
+        throw new NoSuchElementException();
       }
-      else {
-        throw new UnsupportedOperationException();
-      }
-      //if hasNext
-      //return sorted.get[nextIndex++]
-      //else throw exception
+      // if hasNext
+      // return sorted.get[nextIndex++]
+      // else throw exception
     }
-    
+
   }
-  
-  
+
 }
 
-//private class MyListIterator implemnts ListIterator<E>{
- //private dlinkednode nextNode;
- // private int nextIndex;
-  
- // public MyListIterator(){
-  //nextIndex=0
-  //nextNode = traverse[0];
-//}
-  
-  //has next{
-  //nextNode is not null
-  //}
-  
-  //E next(){
-  //if hasnext
-  //remmebr item at nextNode
-  //update nextnode
-  //update nextIndex
-  //return item
-  //if no next, throw nosuchelementexception
-  //}
-  
-  //hasprevious{
-  //if there is a prev of nextNode or nextNode is null
-  //}
-  
-  //E prevoius(){
-  //if hasPrevoius
-  //remembr item at nextNode.prev
-  //update nextNode
-  //update nextIndex
-  //return item
-  // else throw exception
-  //}
-  
-  //prevoiusindex{
-  //return nextIndex-1
-  //}
-  
-  
-  
-  
-//get set remove throws new unsupported 
+// private class MyListIterator implemnts ListIterator<E>{
+// private dlinkednode nextNode;
+// private int nextIndex;
 
-  
-  //private class MySortedIterator implements Iterator<E>{
-  //private sortablelist<E> sorted;
-  //private int nextIndex;
-  
-  //public MySortedIterator(Comparator<E> c){
-  //  init sorted
-  //  copy all items from this into sorted(for int i=0; i<size ; i++) sorted.add(get[i])
-  //  sort sorted (bubble sort)
-  //  nextIndex =0
-  //}
-  
-  //hasNext{
-  //return nextIndex< size
-  //}
-  
-  //E next(){
-  //if hasNext
-  //return sorted.get[nextIndex++]
-  //else throw exception
-  //}
-  
-  //}
-  
-//}
+// public MyListIterator(){
+// nextIndex=0
+// nextNode = traverse[0];
+// }
+
+// has next{
+// nextNode is not null
+// }
+
+// E next(){
+// if hasnext
+// remmebr item at nextNode
+// update nextnode
+// update nextIndex
+// return item
+// if no next, throw nosuchelementexception
+// }
+
+// hasprevious{
+// if there is a prev of nextNode or nextNode is null
+// }
+
+// E prevoius(){
+// if hasPrevoius
+// remembr item at nextNode.prev
+// update nextNode
+// update nextIndex
+// return item
+// else throw exception
+// }
+
+// prevoiusindex{
+// return nextIndex-1
+// }
+
+// get set remove throws new unsupported
+
+// private class MySortedIterator implements Iterator<E>{
+// private sortablelist<E> sorted;
+// private int nextIndex;
+
+// public MySortedIterator(Comparator<E> c){
+// init sorted
+// copy all items from this into sorted(for int i=0; i<size ; i++) sorted.add(get[i])
+// sort sorted (bubble sort)
+// nextIndex =0
+// }
+
+// hasNext{
+// return nextIndex< size
+// }
+
+// E next(){
+// if hasNext
+// return sorted.get[nextIndex++]
+// else throw exception
+// }
+
+// }
+
+// }
