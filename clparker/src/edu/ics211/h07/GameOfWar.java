1a4579964031ff4b6c02d6c5797bae2140a230e7
Cory-PC <school.coryparker@gmail.com>
2021-03-03 11:57:47 -1000

    file init

---
+++ clparker/src/edu/ics211/h07/GameOfWar.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package edu.ics211.h07;
+
+/**
+ * @author Cory Parker
+ *
+ */
+public class GameOfWar implements IGameOfWar {
+
+  /**
+   * 
+   */
+  public GameOfWar() {
+    // TODO Auto-generated constructor stub
+  }
+
+
+  @Override
+  public void initializeGame(Deck d) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+  @Override
+  public boolean playARound() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+
+  @Override
+  public Stack<Card> playerOnesCards() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+  @Override
+  public Stack<Card> playerTwosCards() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+  @Override
+  public Stack<Card> combineStacks(Stack<Card> top, Stack<Card> bottom) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ clparker/src/edu/ics211/h07/Stack.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package edu.ics211.h07;
+
+/**
+ * @author Cory Parker
+ *
+ */
+public class Stack<E> implements IStack211<E> {
+
+  /**
+   * 
+   */
+  public Stack() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public boolean empty() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public E push(E item) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public E peek() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public E pop() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
