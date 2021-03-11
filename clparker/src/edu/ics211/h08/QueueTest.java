149584f5a75f4996eaac83e70902d070b34a0d53
Cory-PC <school.coryparker@gmail.com>
2021-03-10 17:07:32 -1000

    test case updates

---
+++ clparker/src/edu/ics211/h08/QueueTest.java
@@ -0,0 +1,167 @@
+/**
+ * 
+ */
+package edu.ics211.h08;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Cory Parker
+ *
+ */
+class QueueTest {
+  public PacketQueue queue;
+  
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeEach
+  void setUp() throws Exception {
+    queue = new PacketQueue();
+    //Adding to a full queue and removing from an empty queue will throw exceptions –
+    //make sure your JUnit test catches those exceptions.
+  }
+
+  public void addEight() {
+    for(int i=2; i<10; i++) {//should result in 2-8
+      this.queue.add(new Packet(i));
+    }
+    
+    
+    
+  }
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#size()}.
+   */
+  @Test
+  void testSize() {
+    //test empty queue
+    assertEquals(queue.size(), 0);
+    //test queue with one element
+    this.queue.add(new Packet(1));
+    assertEquals(queue.size(), 1);
+    //queue with one empty space
+    addEight();
+    assertEquals(queue.size(),9);
+    //queue that is full
+    this.queue.add(new Packet(10));
+    assertEquals(queue.size(),10);
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#PacketQueue()}.
+   */
+  @Test
+  void testPacketQueue() {
+    PacketQueue tstQueue = new PacketQueue();
+    assertNotNull(tstQueue);
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#offer(edu.ics211.h08.Packet)}.
+   */
+  @Test
+  void testOffer() {
+    //test empty queue
+    assertEquals( this.queue.offer(new Packet(1)), true);
+    assertEquals(this.queue.size(),1);
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#add(edu.ics211.h08.Packet)}.
+   */
+  @Test
+  void testAddPacket() {
+  //test empty queue
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    fail("Not yet implemented");
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#poll()}.
+   */
+  @Test
+  void testPoll() {
+  //test empty queue
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    fail("Not yet implemented");
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#remove()}.
+   */
+  @Test
+  void testRemove() {
+  //test empty queue
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    fail("Not yet implemented");
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#peek()}.
+   */
+  @Test
+  void testPeek() {
+  //test empty queue
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    fail("Not yet implemented");
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#element()}.
+   */
+  @Test
+  void testElement() {
+  //test empty queue
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    fail("Not yet implemented");
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#iterator()}.
+   */
+  @Test
+  void testIterator() {
+  //test empty queue
+    //test queue with one element
+    //queue with one empty space
+    //queue that is full
+    fail("Not yet implemented");
+  }
+
+
+  /**
+   * Test method for {@link edu.ics211.h08.PacketQueue#toString()}.
+   */
+  @Test
+  void testToString() {
+    fail("Not yet implemented");
+  }
+
+}
