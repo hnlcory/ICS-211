dbfd2189533cae5230f7b3c79058c25207e202a5
Cory-PC <school.coryparker@gmail.com>
2021-03-24 17:23:50 -1000

    not good

--- clparker/src/edu/ics211/h09/Sudoku.java
+++ clparker/src/edu/ics211/h09/Sudoku.java
@@ -22,18 +22,13 @@ public class Sudoku {
     // TODO: Implement this method recursively. You may use a recursive
     // helper method.
 
-    //check if base case is invalid
-    if (checkSudoku(sudoku,true)==false) {
+    // check if base case is invalid
+    if (checkSudoku(sudoku, true) == false) {
       return false;
     }
     // scan for the first empty cell
     int[] pos = findEmpty(sudoku);
-    
-    
-    
-    //if no empty cells then return checkSudoku (basecase1)
-    return checkSudoku(sudoku, true);
-   
+
     // if empty cell found
     // get legal values
     // if no legal values found, return false
@@ -45,26 +40,31 @@ public class Sudoku {
     // if out of loop
     // set cell to 0
     // return false
-    // if no empty cells then return checkSudoku (basecase1)
 
-    
+    // if no empty cells then return checkSudoku (basecase1)
+    if (pos == null) {
+      return checkSudoku(sudoku, true);
+    }
+
   }
+
 
   private static int[] findEmpty(int[][] sudoku) {
     for (int i = 0; i < 9; i++) {
       for (int j = 0; j < 9; j++) {
         // if empty cell found
         // get legal values
-        if (sudoku[i][j]==0) {
-          int[] res= {i,j};
+        if (sudoku[i][j] == 0) {
+          int[] res = { i, j };
           return res;
         }
-       
+
       }
     }
-    int[] res= {-1,-1};
-    return res;
+    // int[] res= {-1,-1};
+    return null;
   }
+
 
   /**
    * Find the legal values for the given sudoku and cell.
@@ -75,23 +75,57 @@ public class Sudoku {
    * @return an ArrayList of the valid values.
    */
   public static ArrayList<Integer> legalValues(int[][] sudoku, int row, int column) {
-    // TODO: Implement this method. You may want to look at the checkSudoku method
-    // to see how it finds conflicts.
-
+    boolean valid;
     // check is sudoku [row][column] is 0
+    if (sudoku[row][column] != 0) {
+      return null;
+    }
     // create return list
-    // addetive list
+    ArrayList<Integer> returnList = new ArrayList<Integer>();
+
     // loop over values 1-9
-    // check if value is in the row, if is break
-    // check if value is in column, if is break
-    // check if value is in the 3x3, if is break
-    // add value to return list
+
+    for (int i = 1; i < 10; i++) {
+      valid = true;
+      // check if value is in the row, if is break
+      for (int j = 0; j < 9; j++) {
+        if (i == sudoku[row][j]) {
+          valid = false;
+          break;
+        }
+
+      }
+      // check if value is in column, if is break
+      for (int k = 0; k < 9; k++) {
+        if (i == sudoku[k][column]) {
+          valid = false;
+          break;
+        }
+      }
+      // check if value is in the 3x3, if is break
+      for (int l = 0; l < 3; l++) {
+        for (int m = 0; m < 3; m++) {
+          int testRow = (row / 3 * 3) + l;
+          int testCol = (column / 3 * 3) + m;
+          if (i == sudoku[testRow][testCol]) {
+            valid = false;
+            break;
+          }
+        }
+      }
+
+      // add value to return list
+      if (valid = true) {
+        returnList.add(i);
+      }
+
+    }
+
     // check size of return list, if 0 return null
-    // return list
-
-    // subrtact
-
-    throw new UnsupportedOperationException("legalValues not implemented.");
+    if (returnList.size() == 0) {
+      return null;
+    }
+    return returnList;
   }
 
 
