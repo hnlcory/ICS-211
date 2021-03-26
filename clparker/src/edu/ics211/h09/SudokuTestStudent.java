8a752e0a276073a4ce6c8519983e0f59827481b5
Cory-PC <school.coryparker@gmail.com>
2021-03-26 11:25:17 -1000

    checkstyle updates

--- clparker/src/edu/ics211/h09/SudokuTestStudent.java
+++ clparker/src/edu/ics211/h09/SudokuTestStudent.java
@@ -165,7 +165,7 @@ public class SudokuTestStudent {
     long endTime1 = System.nanoTime();
     System.out.println(endTime1 - startTime1 + " nano sec\n");
     
-    /* AI Escargot */
+    // AI Escargot
     int[][] tstAI = { { 1, 0, 0, 0, 0, 7, 0, 9, 0 }, { 0, 3, 0, 0, 2, 0, 0, 0, 8 },
         { 0, 0, 9, 6, 0, 0, 5, 0, 0 }, { 0, 0, 5, 3, 0, 0, 9, 0, 0 },
         { 0, 1, 0, 0, 8, 0, 0, 0, 2 }, { 6, 0, 0, 0, 0, 4, 0, 0, 0 },
