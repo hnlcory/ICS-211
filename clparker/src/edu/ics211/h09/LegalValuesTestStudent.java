66a8ec0d6cf8462299ff309933ee5bc86b8b301b
Cory-PC <school.coryparker@gmail.com>
2021-03-26 11:17:26 -1000

    checkstyle fixes

--- clparker/src/edu/ics211/h09/LegalValuesTestStudent.java
+++ clparker/src/edu/ics211/h09/LegalValuesTestStudent.java
@@ -7,11 +7,9 @@
 package edu.ics211.h09;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -30,9 +28,11 @@ class LegalValuesTestStudent {
     ArrayList<Integer> intArray = new ArrayList<Integer>();
     intArray.add(7);
 
-    int[][] test = { { 0, 8, 3, 4, 9, 6, 1, 2, 5 }, { 1, 2, 5, 7, 3, 8, 9, 6, 4 }, { 4, 6, 9, 2, 5, 1, 7, 3, 8 },
-        { 2, 3, 6, 1, 8, 5, 4, 7, 9 }, { 5, 1, 4, 6, 7, 9, 3, 8, 2 }, { 9, 7, 8, 3, 4, 2, 5, 1, 6 },
-        { 8, 5, 7, 9, 6, 3, 2, 4, 1 }, { 6, 4, 1, 5, 2, 7, 8, 9, 3 }, { 3, 9, 2, 8, 1, 4, 6, 5, 7 } };
+    int[][] test = { { 0, 8, 3, 4, 9, 6, 1, 2, 5 }, { 1, 2, 5, 7, 3, 8, 9, 6, 4 },
+        { 4, 6, 9, 2, 5, 1, 7, 3, 8 }, { 2, 3, 6, 1, 8, 5, 4, 7, 9 },
+        { 5, 1, 4, 6, 7, 9, 3, 8, 2 }, { 9, 7, 8, 3, 4, 2, 5, 1, 6 },
+        { 8, 5, 7, 9, 6, 3, 2, 4, 1 }, { 6, 4, 1, 5, 2, 7, 8, 9, 3 }, 
+        { 3, 9, 2, 8, 1, 4, 6, 5, 7 } };
     // test for legal number
     ArrayList<Integer> legal = Sudoku.legalValues(test, 0, 0);
     assertEquals(legal.get(0), intArray.get(0));
--- clparker/src/edu/ics211/h09/SudokuTest.java
+++ clparker/src/edu/ics211/h09/SudokuTest.java
@@ -1,11 +1,9 @@
 package edu.ics211.h09;
 
 /**
- * Test a Sudoku solver
+ * Test a Sudoku solver.
  * 
  * @author Biagioni, Edoardo and Cam Moore
- * @date October 23, 2013
- * @bugs none
  */
 public class SudokuTest {
 
@@ -27,7 +25,7 @@ public class SudokuTest {
 
 
   /**
-   * Test whether two sudoku are equal. If not, return a new sudoku that is blank where the two sudoku differ.
+   * Test whether two sudoku are equal.
    * 
    * @param the sudoku to be checked
    * @param the solution checked
@@ -57,7 +55,7 @@ public class SudokuTest {
 
 
   /**
-   * Try to solve a sudoku. If a solution is provided, also check against the solution. Print the results.
+   * Try to solve a sudoku.
    * 
    * @param the name of this sudoku
    * @param the sudoku to be solved
@@ -77,43 +75,58 @@ public class SudokuTest {
         }
       } else { /* the supposed solution is not a complete or valid sudoku */
         if (!isFilled(sudoku)) {
-          System.out.println("sudoku was not completely filled:\n" + Sudoku.toString(sudoku, false));
+          System.out.println("not completely filled:\n" + Sudoku.toString(sudoku, false));
         }
         if (!Sudoku.checkSudoku(sudoku, false)) {
           System.out.println("sudoku is not a valid solution:\n" + Sudoku.toString(sudoku, false));
         }
       }
     } else {
-      System.out.println("unable to complete sudoku " + name + "\n" + Sudoku.toString(sudoku, true));
+      System.out.println("unable to complete" + name + "\n" + Sudoku.toString(sudoku, true));
     }
   }
 
 
+  /**
+   * Main for testing sudoku puzzles.
+   * 
+   * @author Biagioni, Edoardo and Cam Moore, added by Cory Parker
+   * @param arg arguments
+   */
   public static void main(String[] arg) {
-    int[][] example = { { 7, 8, 0, 0, 9, 0, 0, 2, 0 }, { 1, 0, 0, 0, 0, 0, 9, 6, 4 }, { 0, 0, 0, 2, 5, 1, 0, 0, 0 },
-        { 0, 0, 6, 1, 8, 5, 0, 0, 0 }, { 5, 0, 4, 0, 0, 0, 3, 0, 2 }, { 0, 0, 0, 3, 4, 2, 5, 0, 0 },
-        { 0, 0, 0, 9, 6, 3, 0, 0, 0 }, { 6, 4, 1, 0, 0, 0, 0, 0, 3 }, { 0, 9, 0, 0, 1, 0, 0, 5, 7 } };
+    int[][] example = { { 7, 8, 0, 0, 9, 0, 0, 2, 0 }, { 1, 0, 0, 0, 0, 0, 9, 6, 4 }, 
+        { 0, 0, 0, 2, 5, 1, 0, 0, 0 }, { 0, 0, 6, 1, 8, 5, 0, 0, 0 },
+        { 5, 0, 4, 0, 0, 0, 3, 0, 2 }, { 0, 0, 0, 3, 4, 2, 5, 0, 0 },
+        { 0, 0, 0, 9, 6, 3, 0, 0, 0 }, { 6, 4, 1, 0, 0, 0, 0, 0, 3 },
+        { 0, 9, 0, 0, 1, 0, 0, 5, 7 } };
 
-    int[][] solution = { { 7, 8, 3, 4, 9, 6, 1, 2, 5 }, { 1, 2, 5, 7, 3, 8, 9, 6, 4 }, { 4, 6, 9, 2, 5, 1, 7, 3, 8 },
-        { 2, 3, 6, 1, 8, 5, 4, 7, 9 }, { 5, 1, 4, 6, 7, 9, 3, 8, 2 }, { 9, 7, 8, 3, 4, 2, 5, 1, 6 },
-        { 8, 5, 7, 9, 6, 3, 2, 4, 1 }, { 6, 4, 1, 5, 2, 7, 8, 9, 3 }, { 3, 9, 2, 8, 1, 4, 6, 5, 7 } };
+    int[][] solution = { { 7, 8, 3, 4, 9, 6, 1, 2, 5 }, { 1, 2, 5, 7, 3, 8, 9, 6, 4 },
+        { 4, 6, 9, 2, 5, 1, 7, 3, 8 }, { 2, 3, 6, 1, 8, 5, 4, 7, 9 },
+        { 5, 1, 4, 6, 7, 9, 3, 8, 2 }, { 9, 7, 8, 3, 4, 2, 5, 1, 6 },
+        { 8, 5, 7, 9, 6, 3, 2, 4, 1 }, { 6, 4, 1, 5, 2, 7, 8, 9, 3 },
+        { 3, 9, 2, 8, 1, 4, 6, 5, 7 } };
 
-    int[][] example2 = { { 0, 6, 0, 9, 0, 8, 0, 1, 0 }, { 0, 0, 4, 0, 0, 0, 0, 0, 0 }, { 8, 0, 3, 0, 0, 0, 4, 5, 0 },
-        { 2, 0, 0, 0, 6, 0, 0, 0, 8 }, { 9, 0, 0, 0, 0, 0, 0, 0, 4 }, { 5, 0, 0, 0, 7, 0, 0, 0, 2 },
-        { 0, 7, 8, 0, 0, 0, 9, 0, 5 }, { 0, 0, 0, 0, 0, 0, 6, 0, 0 }, { 0, 1, 0, 3, 0, 2, 0, 4, 0 } };
+    int[][] example2 = { { 0, 6, 0, 9, 0, 8, 0, 1, 0 }, { 0, 0, 4, 0, 0, 0, 0, 0, 0 },
+        { 8, 0, 3, 0, 0, 0, 4, 5, 0 }, { 2, 0, 0, 0, 6, 0, 0, 0, 8 },
+        { 9, 0, 0, 0, 0, 0, 0, 0, 4 }, { 5, 0, 0, 0, 7, 0, 0, 0, 2 },
+        { 0, 7, 8, 0, 0, 0, 9, 0, 5 }, { 0, 0, 0, 0, 0, 0, 6, 0, 0 },
+        { 0, 1, 0, 3, 0, 2, 0, 4, 0 } };
 
-    int[][] solution2 = { { 7, 6, 5, 9, 4, 8, 2, 1, 3 }, { 1, 2, 4, 5, 3, 6, 7, 8, 9 }, { 8, 9, 3, 7, 2, 1, 4, 5, 6 },
-        { 2, 4, 7, 1, 6, 3, 5, 9, 8 }, { 9, 3, 6, 2, 8, 5, 1, 7, 4 }, { 5, 8, 1, 4, 7, 9, 3, 6, 2 },
-        { 3, 7, 8, 6, 1, 4, 9, 2, 5 }, { 4, 5, 2, 8, 9, 7, 6, 3, 1 }, { 6, 1, 9, 3, 5, 2, 8, 4, 7 } };
+    int[][] solution2 = { { 7, 6, 5, 9, 4, 8, 2, 1, 3 }, { 1, 2, 4, 5, 3, 6, 7, 8, 9 },
+        { 8, 9, 3, 7, 2, 1, 4, 5, 6 }, { 2, 4, 7, 1, 6, 3, 5, 9, 8 },
+        { 9, 3, 6, 2, 8, 5, 1, 7, 4 }, { 5, 8, 1, 4, 7, 9, 3, 6, 2 },
+        { 3, 7, 8, 6, 1, 4, 9, 2, 5 }, { 4, 5, 2, 8, 9, 7, 6, 3, 1 },
+        { 6, 1, 9, 3, 5, 2, 8, 4, 7 } };
 
     /* a hard sudoku known as AI Escargot */
-    int[][] example3 = { { 1, 0, 0, 0, 0, 7, 0, 9, 0 }, { 0, 3, 0, 0, 2, 0, 0, 0, 8 }, { 0, 0, 9, 6, 0, 0, 5, 0, 0 },
-        { 0, 0, 5, 3, 0, 0, 9, 0, 0 }, { 0, 1, 0, 0, 8, 0, 0, 0, 2 }, { 6, 0, 0, 0, 0, 4, 0, 0, 0 },
-        { 3, 0, 0, 0, 0, 0, 0, 1, 0 }, { 0, 4, 0, 0, 0, 0, 0, 0, 7 }, { 0, 0, 7, 0, 0, 0, 3, 0, 0 } };
+    int[][] example3 = { { 1, 0, 0, 0, 0, 7, 0, 9, 0 }, { 0, 3, 0, 0, 2, 0, 0, 0, 8 },
+        { 0, 0, 9, 6, 0, 0, 5, 0, 0 }, { 0, 0, 5, 3, 0, 0, 9, 0, 0 },
+        { 0, 1, 0, 0, 8, 0, 0, 0, 2 }, { 6, 0, 0, 0, 0, 4, 0, 0, 0 },
+        { 3, 0, 0, 0, 0, 0, 0, 1, 0 }, { 0, 4, 0, 0, 0, 0, 0, 0, 7 },
+        { 0, 0, 7, 0, 0, 0, 3, 0, 0 } };
 
     testSudoku("example 1", example, solution);
     testSudoku("example 2", example2, solution2);
     testSudoku("AI Escargot", example3, null);
-
   }
 }
