dd5b1b445177135de32283ff5bd6d13c9a45195c
Cory-Mac <school.coryparker@gmail.com>
2021-04-15 18:27:33 -1000

    updates

---
+++ clparker/src/edu/ics211/h11/HuffmanJUnitTest.java
@@ -0,0 +1,74 @@
+package edu.ics211.h11;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HuffmanJUnitTest {
+
+  @BeforeEach
+  void setUp() throws Exception {
+  }
+
+
+  @Test
+  void testHuffmanByteArray() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testHuffmanBitReader() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testDecode() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testEncode() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testWrite() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testToString() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testCompressString() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testCompressInputStreamOutputStream() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testDecompressString() {
+    fail("Not yet implemented");
+  }
+
+
+  @Test
+  void testDecompressInputStreamOutputStream() {
+    fail("Not yet implemented");
+  }
+
+}
