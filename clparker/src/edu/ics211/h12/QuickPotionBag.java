5d25912db54b0de2f5a2d84428cf18b44e6a1152
Cory Parker <school.coryparker@gmail.com>
2021-04-28 09:36:16 -1000

    init

---
+++ clparker/src/edu/ics211/h12/QuickPotionBag.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package edu.ics211.h12;
+
+/**
+ * @author Cory-PC
+ *
+ */
+public class QuickPotionBag implements PotionBag {
+
+  @Override
+  public void store(Potion p) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+  @Override
+  public long timedStore(Potion p) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+
+  @Override
+  public Potion retrieve(Potion p) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+  @Override
+  public long timedRetrieve(Potion p) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
