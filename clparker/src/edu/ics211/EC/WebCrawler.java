2eb8f14584444e97e87971c5e453414728702ddd
Cory Parker <school.coryparker@gmail.com>
2021-04-28 20:57:27 -1000

    working address validation

--- clparker/src/edu/ics211/EC/WebCrawler.java
+++ clparker/src/edu/ics211/EC/WebCrawler.java
@@ -6,6 +6,8 @@ package edu.ics211.EC;
 import java.util.Scanner;
 
 /**
+ * Web Crawler.
+ * 
  * @author Cory Parker
  *
  */
@@ -13,19 +15,33 @@ public class WebCrawler {
   String address;
 
   /**
+   * Initalize Variables.
+   * 
+   * @author Cory Parker.
    * 
    */
   public WebCrawler() {
     address = new String();
   }
 
-  // confirms valid address, return true or false
+
+  /**
+   * confirms valid address, return true or false.
+   * 
+   * @param s string to confirm.
+   * @return true if good address or false
+   */
   public Boolean checkAddress(String s) {
     // given address
     // if string does contin .com, etc return true
     if (s.contains(".com") || s.contains(".net") || s.contains(".edu") || s.contains(".mil") || s.contains(".org")
         || s.contains(".gov")) {
-      return true;
+      // check for https and ://
+      if (s.contains("https") || s.contains("http") && s.contains("://")) {
+        return true;
+      } else {
+        return false;
+      }
     }
     // else return false
     else {
@@ -35,7 +51,11 @@ public class WebCrawler {
   }
 
 
-  // get address in console and return if valid address
+  /**
+   * gets address from console.
+   * 
+   * @return if address is valid or prompt user
+   */
   public String getAddress() {
     Boolean validAddress = false;
     Scanner inputScan = new Scanner(System.in);
@@ -43,13 +63,13 @@ public class WebCrawler {
       System.out.println("Enter Valid Web Address: ");
       this.address = inputScan.nextLine();
 
-      //if address is formatted properly, set valid to true and return
-      if (checkAddress(this.address)==true) {
-        inputScan.close(); 
-        validAddress=true;
+      // if address is formatted properly, set valid to true and return **save crawl depth
+      if (checkAddress(this.address) == true) {
+        inputScan.close();
+        validAddress = true;
         break;
       }
-      //else wrong format, loop on
+      // else wrong format, loop on
       System.out.println("Error : invalid web address\nUsage: [Valid Web Address] [depth to be searched]\n");
     }
     return address;
